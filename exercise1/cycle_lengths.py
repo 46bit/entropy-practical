9f12a4e33487a3a1216b79fdb0e0854f1f025746
Michael Mokrysz <hi@46b.it>
2016-08-03 12:15:46 +0100

    Added obfuscated tests to cycle lengths. Bcrypt with work factor of 14, to ensure bruteforcing isn't a viable strategy in a hurry.

--- exercise1/cycle_lengths.py
+++ exercise1/cycle_lengths.py
@@ -63,26 +63,54 @@ for lcg_name in ["A", "B", "C", "D", "E", "F", "G", "H"]:
     print("LCG %s\n  %s\n  cycle_length=%d" % (lcg_name, lcg.param, find_cycle_length(lcg)))
 
 # @TODO: Move to separate file before giving to student.
-# @TODO: Obfuscate correct answers before giving to student.
-import unittest
+import unittest, bcrypt
 
 class LCGCycleLengthTest(unittest.TestCase):
+    # Bruteforcing for the correct cycle lengths is going to take a while. Each guess will
+    # take you >1s on a typical CPU, and the values range into the tens of thousands.
+    # Given that, you're probably better off asking for help instead. ;-)
+    def bcrypt_cmp(self, expectation, cycle_length):
+        bcrypted_cycle_length = bcrypt.hashpw(str(cycle_length), expectation)
+        self.assertEqual(expectation, bcrypted_cycle_length)
+
     def test_a(self):
-        self.assertEqual(find_cycle_length(LCGS["A"]), 1024)
+        expectation = "$2b$14$6kv3i0cEBc7oSOXIdrsjquc66kOZM7.6XM6aINT7Hp0kk2fmq1TZ2"
+        cycle_length = find_cycle_length(LCGS["A"])
+        self.bcrypt_cmp(expectation, cycle_length)
+
     def test_b(self):
-        self.assertEqual(find_cycle_length(LCGS["B"]), 150)
+        expectation = "$2b$14$AYdQToXcR8oJTZw7DZvgGO9jU72HVsSlsuHljI89wUec46mR37lV2"
+        cycle_length = find_cycle_length(LCGS["B"])
+        self.bcrypt_cmp(expectation, cycle_length)
+
     def test_c(self):
-        self.assertEqual(find_cycle_length(LCGS["C"]), 3620)
+        expectation = "$2b$14$H4sH1OjafANOmPdODmCGj.enbZPb8Yezfs3n13rn1k7iK0/cKblB6"
+        cycle_length = find_cycle_length(LCGS["C"])
+        self.bcrypt_cmp(expectation, cycle_length)
+
     def test_d(self):
-        self.assertEqual(find_cycle_length(LCGS["D"]), 162)
+        expectation = "$2b$14$yDDSQglGxtYSE4wUQV094u7ewL.O48Qpy4SyCKDUwEYCabMrMQXBG"
+        cycle_length = find_cycle_length(LCGS["D"])
+        self.bcrypt_cmp(expectation, cycle_length)
 
     def test_e(self):
-        self.assertEqual(find_cycle_length(LCGS["E"]), 450)
+        expectation = "$2b$14$wrB9bnwIlK21LVeDToiuq.I8nU4XHhp4kDTAmE5pZtmQyfZ2hvVSC"
+        cycle_length = find_cycle_length(LCGS["E"])
+        self.bcrypt_cmp(expectation, cycle_length)
+
     def test_f(self):
-        self.assertEqual(find_cycle_length(LCGS["F"]), 125)
+        expectation = "$2b$14$998iGLzdoWhAiqgJgxqGK.w3raYScYgaHALtuvhvtdsKv6A7mMP.K"
+        cycle_length = find_cycle_length(LCGS["F"])
+        self.bcrypt_cmp(expectation, cycle_length)
+
     def test_g(self):
-        self.assertEqual(find_cycle_length(LCGS["G"]), 65536)
+        expectation = "$2b$14$buoqYdqykxUlC3yYVOdk1.m3EM3bgg4Qihyyg0m0.ZSaACiOhAeca"
+        cycle_length = find_cycle_length(LCGS["G"])
+        self.bcrypt_cmp(expectation, cycle_length)
+
     def test_h(self):
-        self.assertEqual(find_cycle_length(LCGS["H"]), 58564)
+        expectation = "$2b$14$x/v4ZI4/wpvefetwp.PtmuLr//.SMXHOCzl0.Er9B6jhDQgj7azky"
+        cycle_length = find_cycle_length(LCGS["H"])
+        self.bcrypt_cmp(expectation, cycle_length)
 
 unittest.main()
