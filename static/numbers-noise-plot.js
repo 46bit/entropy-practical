04e34cd8c09fe76b715b5f3f854f7d7fda33bac6
Michael Mokrysz <hi@46b.it>
2016-11-09 02:51:01 +0000

    Github pages fix: removed underscores from folder names.

--- static/numbers-noise-plot.js
+++ static/numbers-noise-plot.js
@@ -1,6 +1,6 @@
-// <canvas class="numbers-numbers" id="numbers-numbers-naturals" width="900" height="790" data-numbers="_static/65536-naturals.txt"></canvas>
+// <canvas class="numbers-numbers" id="numbers-numbers-naturals" width="900" height="790" data-numbers="static/65536-naturals.txt"></canvas>
 
-function NumbersNoiseCoordlot(canvas_id) {
+function NumbersNoisePlot(canvas_id) {
   var _self = this
 
   _self.canvas = document.getElementById(canvas_id)
@@ -21,7 +21,7 @@ function NumbersNoiseCoordlot(canvas_id) {
   })
 }
 
-NumbersNoiseCoordlot.prototype.processNumberDump = function processNumberDump(number_dump) {
+NumbersNoisePlot.prototype.processNumberDump = function processNumberDump(number_dump) {
   var _self = this
 
   var number_strings = number_dump.split("\n"),
@@ -33,14 +33,9 @@ NumbersNoiseCoordlot.prototype.processNumberDump = function processNumberDump(nu
     _self.numbers.push(parseInt(number_str))
   }
   _self.maxNumber = Math.max.apply(null, _self.numbers)
-
-  _self.scale = Math.max(1.0, Math.max(
-    parseFloat(_self.width) / _self.maxNumber,
-    parseFloat(_self.height) / _self.maxNumber
-  ))
 }
 
-NumbersNoiseCoordlot.prototype.drawNumbers = function drawNumbers() {
+NumbersNoisePlot.prototype.drawNumbers = function drawNumbers() {
   var _self = this
 
   // http://stackoverflow.com/questions/4899799/whats-the-best-way-to-set
@@ -49,16 +44,17 @@ NumbersNoiseCoordlot.prototype.drawNumbers = function drawNumbers() {
       d = p.data
   d[3] = 255
 
-  for (var i = 0; i + 1 < _self.numbers.length; i++) {
-    var proportion = parseFloat(i) / _self.numbers.length
-    d[0] = d[1] = d[2] = 255 - (55 + parseInt(Math.round(proportion * 200)))
-    var x = (_self.numbers[i] % _self.width) * _self.scale,
-        y = (_self.numbers[i+1] % _self.height) * _self.scale,
-        dx, dy
-
-    for (dx = 0; dx < _self.scale; dx++) {
-      for (dy = 0; dy < _self.scale; dy++) {
-        _self.context.putImageData(p, parseInt(Math.round(x + dx)), parseInt(Math.round(y + dy)) + 25)
+  var i = 0
+  for (var y = 0; y < _self.height; y++) {
+    for (var x = 0; x < _self.width; x++) {
+      var brightness = 255 - parseInt(Math.round(255 * (_self.numbers[i] / _self.maxNumber)))
+      d[0] = d[1] = d[2] = brightness
+      _self.context.putImageData(p, x, y + 25)
+      i++
+      if (i >= _self.numbers.length) {
+        y = _self.height
+        x = _self.width
+        break
       }
     }
   }
@@ -70,15 +66,11 @@ NumbersNoiseCoordlot.prototype.drawNumbers = function drawNumbers() {
   _self.context.fillStyle = "rgb(255, 255, 255)"
   _self.context.fillRect(0, 0, _self.width, 25)
   _self.context.fillStyle = "rgb(0, 0, 0)"
-  var plot_name_out = _self.plot_name
-  if (_self.scale > 1.1) {
-    plot_name_out += " (rescaled)"
-  }
-  _self.context.fillText(plot_name_out, _self.width / 2, 5)
+  _self.context.fillText(_self.plot_name, _self.width / 2, 5)
 }
 
 jQuery(document).ready(function () {
-  $(".numbers-noise-coord-plot").each(function () {
-    new NumbersNoiseCoordlot(this.id)
+  $(".numbers-noise-plot").each(function () {
+    new NumbersNoisePlot(this.id)
   })
 })
--- static/numbers-phase-space-plot.js
+++ static/numbers-phase-space-plot.js
@@ -1,6 +1,6 @@
-// <canvas class="numbers-numbers" id="numbers-numbers-naturals" width="900" height="790" data-numbers-path="_static/65536-naturals.txt"></canvas>
+// <canvas class="numbers-numbers" id="numbers-numbers-naturals" width="900" height="790" data-numbers="static/65536-naturals.txt"></canvas>
 
-function NumbersXYZPlot(container) {
+function NumbersPhaseSpacePlot(container) {
   var _self = this
 
   _self.container = container
@@ -8,7 +8,7 @@ function NumbersXYZPlot(container) {
 
   _self.width = _self.$container.innerWidth()
   _self.height = _self.$container.innerHeight()
-  _self.numberCapacity = 32000
+  _self.numberCapacity = 32000 //_self.width * _self.height
 
   _self.numbers_path = _self.$container.attr("data-numbers-path")
   jQuery.get(_self.numbers_path, function(number_dump) {
@@ -20,7 +20,7 @@ function NumbersXYZPlot(container) {
   })
 }
 
-NumbersXYZPlot.prototype.initialiseRenderer = function initialiseRenderer() {
+NumbersPhaseSpacePlot.prototype.initialiseRenderer = function initialiseRenderer() {
   var _self = this
 
   _self.renderer = new THREE.WebGLRenderer({antialias: true})
@@ -34,20 +34,20 @@ NumbersXYZPlot.prototype.initialiseRenderer = function initialiseRenderer() {
   var ambient_light = new THREE.AmbientLight(0xffffff)
   _self.scene.add(ambient_light)
 
-  /*var frustumSize = 2,
+  var frustumSize = 600,
       aspect = parseFloat(_self.width) / _self.height
   _self.camera = new THREE.OrthographicCamera(
     0.5 * frustumSize * aspect / - 2,
     0.5 * frustumSize * aspect / 2,
     frustumSize / 2,
     frustumSize / -2,
-    0.3,
-    15
-  )*/
+    150,
+    1000
+  )
 
-  _self.camera = new THREE.PerspectiveCamera(60, _self.width / _self.height, 1, 10000)
-  _self.camera.position.set(20, 20, 20)//(2.0, 2.0, 2.0)
-  _self.camera.lookAt(new THREE.Vector3(0.5, 0.5, 0.5))
+  //_self.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000)
+  _self.camera.position.set(500, 800, 0)
+  _self.camera.lookAt(new THREE.Vector3())
 
   _self.controls = new THREE.OrbitControls(_self.camera, _self.renderer.domElement)
   _self.controls.enableDamping = true
@@ -55,19 +55,19 @@ NumbersXYZPlot.prototype.initialiseRenderer = function initialiseRenderer() {
   _self.controls.enableZoom = true
   _self.controls.addEventListener("change", _self.render.bind(_self))
 
-  _self.voxel_geometry = new THREE.BoxGeometry(1, 1, 1)
+  _self.voxel_geometry = new THREE.BoxGeometry(0.01, 0.01, 0.01)
   _self.voxel_material = new THREE.MeshLambertMaterial({
-    color: 0xffffff//000000
+    color: 0xffffff
   })
 }
 
-NumbersXYZPlot.prototype.render = function render() {
+NumbersPhaseSpacePlot.prototype.render = function render() {
   var _self = this
 
   _self.renderer.render(_self.scene, _self.camera)
 }
 
-NumbersXYZPlot.prototype.animate = function animate() {
+NumbersPhaseSpacePlot.prototype.animate = function animate() {
   var _self = this
 
   requestAnimationFrame(animate.bind(_self))
@@ -75,7 +75,7 @@ NumbersXYZPlot.prototype.animate = function animate() {
   _self.render()
 }
 
-NumbersXYZPlot.prototype.processNumberDump = function processNumberDump(number_dump) {
+NumbersPhaseSpacePlot.prototype.processNumberDump = function processNumberDump(number_dump) {
   var _self = this
 
   var number_strings = number_dump.split("\n"),
@@ -90,16 +90,16 @@ NumbersXYZPlot.prototype.processNumberDump = function processNumberDump(number_d
   _self.numbersLength = _self.numbers.length
 }
 
-NumbersXYZPlot.prototype.drawNumbers = function drawNumbers() {
+NumbersPhaseSpacePlot.prototype.drawNumbers = function drawNumbers() {
   var _self = this
 
   _self.xs = []
   _self.ys = []
   _self.zs = []
-  for (var n = 0; n + 2 < _self.numbersLength; n++) {
-    _self.xs.push(_self.numbers[n])
-    _self.ys.push(_self.numbers[n+1])
-    _self.zs.push(_self.numbers[n+2])
+  for (var n = 3; n < _self.numbersLength; n++) {
+    _self.xs.push(-parseFloat(_self.maxNumber) / 2 + _self.numbers[n-2] - _self.numbers[n-3])
+    _self.ys.push(-parseFloat(_self.maxNumber) / 2 + _self.numbers[n-1] - _self.numbers[n-2])
+    _self.zs.push(-parseFloat(_self.maxNumber) / 2 + _self.numbers[n] - _self.numbers[n-1])
   }
 
   _self.xBounds = _self.getArrayBounds(_self.xs)
@@ -113,9 +113,7 @@ NumbersXYZPlot.prototype.drawNumbers = function drawNumbers() {
   var voxels_geometry = new THREE.Geometry()
   for (var i = 0; i < _self.numbersLength; i++) {
     var voxel = new THREE.Mesh(_self.voxel_geometry)
-    //voxel.position.set(1.0 / _self.xsRescaled[i], 1.0 / _self.ysRescaled[i], 1.0 / _self.zsRescaled[i])
-    voxel.position.set(_self.xsRescaled[i], _self.ysRescaled[i], _self.zsRescaled[i])
-    //console.log(voxel.position)
+    voxel.position.set(_self.xs[i] / 100.0, _self.ys[i] / 100.0, _self.zs[i] / 100.0)
     voxel.updateMatrix()
     voxels_geometry.merge(voxel.geometry, voxel.matrix)
   }
@@ -123,7 +121,7 @@ NumbersXYZPlot.prototype.drawNumbers = function drawNumbers() {
   _self.scene.add(voxels_mesh)
 }
 
-NumbersXYZPlot.prototype.getArrayBounds = function getArrayBounds(a) {
+NumbersPhaseSpacePlot.prototype.getArrayBounds = function getArrayBounds(a) {
   var _self = this
 
   var stats = [Math.min.apply(null, a), Math.max.apply(null, a)]
@@ -132,22 +130,18 @@ NumbersXYZPlot.prototype.getArrayBounds = function getArrayBounds(a) {
   return stats
 }
 
-NumbersXYZPlot.prototype.rescaleArray = function rescaleArray(a) {
+NumbersPhaseSpacePlot.prototype.rescaleArray = function rescaleArray(a) {
   var _self = this
 
   var bounds = _self.getArrayBounds(a)
   for (var i = 0; i < a.length; i++) {
-    a[i] = a[i] / bounds[1]
-    /*a[i] = (a[i] - bounds[2]) * bounds[3]
-    if (a[i] == 0) {
-      a[i] = 1.0 / a[i]
-    }*/
+    a[i] = (a[i] - bounds[2]) * bounds[3]
   }
   return a
 }
 
 jQuery(document).ready(function () {
-  $(".numbers-xyz-plot").each(function () {
-    window.xyz_plot = new NumbersXYZPlot(this)
+  $(".numbers-phase-space-plot").each(function () {
+    new NumbersPhaseSpacePlot(this)
   })
 })
